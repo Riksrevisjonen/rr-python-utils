3b8fb5410aaeb258486d90a55c7b20f4b4c4d263
Aleksander Eilertsen <ale@riksrevisjonen.no>
2022-06-13 16:28:49 +0200

    Revert adding save_netcdf_to_blob()

--- rrutils/azure.py
+++ rrutils/azure.py
@@ -1,6 +1,6 @@
 from azure.storage.blob import BlobClient, ContainerClient
-from tempfile import NamedTemporaryFile
-import xarray
+#from tempfile import NamedTemporaryFile
+#import xarray
 import logging
 
 
@@ -184,67 +184,64 @@ def save_to_blob(
         logging.error('An error occured while trying to upload {0}'.format(blob_name))
 
 
-def save_netcdf_to_blob(
-        data: object, 
-        container_name: str, 
-        blob_name: str,
-        account_url: str = None,
-        credential: object = None,
-        conn_str: str = None, 
-        verbose: bool = False, 
-        **kwargs):
+# def save_netcdf_to_blob(
+#         data: object, 
+#         container_name: str, 
+#         blob_name: str,
+#         account_url: str = None,
+#         credential: object = None,
+#         conn_str: str = None, 
+#         verbose: bool = False, 
+#         **kwargs):
 
-    """Save NETCDF to blob
+#     """Save NETCDF to blob
     
-    Saves a NETCDF data object to an Azure blob storage.
+#     Saves a NETCDF data object to an Azure blob storage.
 
-    Args: 
-        data: Data object
-        container_name: Blob container 
-        blob_name: Blob name
-        account_url: Account URL. Use together with credential
-        credential: Credential. Use together with account_url 
-        conn_str: Connection string 
-        verbose: If True additional information is printed to the console
-        **kwargs: Additional arguments passed to upload_blob()
-    """
+#     Args: 
+#         data: Data object
+#         container_name: Blob container 
+#         blob_name: Blob name
+#         account_url: Account URL. Use together with credential
+#         credential: Credential. Use together with account_url 
+#         conn_str: Connection string 
+#         verbose: If True additional information is printed to the console
+#         **kwargs: Additional arguments passed to upload_blob()
+#     """
 
-    if verbose: logging.info('Connecting to blob...')
-    if conn_str == None: 
-        if account_url is None or credential is None:
-            logging.error('Both account url and credential must be provided!')
-            return
-        # if not isinstance(credential, DefaultAzureCredential):
-        #    logging.error('You must use a valid Azure credential!')
-        blob = BlobClient(
-            account_url=account_url,
-            credential=credential, 
-            container_name=container_name,
-            blob_name=blob_name)
-    else: 
-        if conn_str is None:
-            logging.error('Connection string cannot be empty!')
-            return        
-        blob = BlobClient.from_connection_string(
-            conn_str=conn_str,
-            container_name=container_name,
-            blob_name=blob_name)
-    if blob.exists():
-        logging.warn('Blob {0} already exists. Skipping upload'.format(blob_name))
-    try: 
-        if verbose: logging.info('Uploading blob...')
-        # Save to blob (by first writing a tmp-file to disk)
-        with NamedTemporaryFile(suffix = '.nc') as tmp: 
-            # Set encoding 
-            comp = dict(zlib = True, complevel = 5)
-            encoding = {var: comp for var in data.data_vars}
-            # Save to tempfile 
-            data.to_netcdf(tmp.name, encoding = encoding, engine = 'netcdf4')
-            with open(tmp.name, 'rb') as d:
-                blob.upload_blob(d, **kwargs)
-            tmp.close()
-    except:
-        logging.error('An error occured while trying to upload {0}'.format(blob_name))
-
-
-
+#     if verbose: logging.info('Connecting to blob...')
+#     if conn_str == None: 
+#         if account_url is None or credential is None:
+#             logging.error('Both account url and credential must be provided!')
+#             return
+#         # if not isinstance(credential, DefaultAzureCredential):
+#         #    logging.error('You must use a valid Azure credential!')
+#         blob = BlobClient(
+#             account_url=account_url,
+#             credential=credential, 
+#             container_name=container_name,
+#             blob_name=blob_name)
+#     else: 
+#         if conn_str is None:
+#             logging.error('Connection string cannot be empty!')
+#             return        
+#         blob = BlobClient.from_connection_string(
+#             conn_str=conn_str,
+#             container_name=container_name,
+#             blob_name=blob_name)
+#     if blob.exists():
+#         logging.warn('Blob {0} already exists. Skipping upload'.format(blob_name))
+#     try: 
+#         if verbose: logging.info('Uploading blob...')
+#         # Save to blob (by first writing a tmp-file to disk)
+#         with NamedTemporaryFile(suffix = '.nc') as tmp: 
+#             # Set encoding 
+#             comp = dict(zlib = True, complevel = 5)
+#             encoding = {var: comp for var in data.data_vars}
+#             # Save to tempfile 
+#             data.to_netcdf(tmp.name, encoding = encoding, engine = 'netcdf4')
+#             with open(tmp.name, 'rb') as d:
+#                 blob.upload_blob(d, **kwargs)
+#             tmp.close()
+#     except:
+#         logging.error('An error occured while trying to upload {0}'.format(blob_name))
